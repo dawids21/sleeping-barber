a4ed38ffc71485e38ccfb1e5e37291e857141e7b
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-01-06 18:25:54 +0100

    create hairdressers and clients in loop

--- fryzjerzy.c
+++ fryzjerzy.c
@@ -31,74 +31,78 @@ int main(int argc, char const *argv[]) {
     int change_queue = new_change_queue();
 
     // HAIRDRESSER
-    int hairdresser_id = 0;
-    int client_id = 1;
     log_num("start main", getpid());
-    if (fork() == 0) {
-        srand(getpid());
-        log_num("start hairdresser", getpid());
-        for (int i = 0; i < 100; i++) {
-            usleep(rand() % 500000);
-            client client = take_client(waiting_room);
-            log_num("get client", client.id);
-            money to_pay;
-            if (rand() % 2 == 0) {
-                to_pay = count_minimum_coins(client.money, COST_PER_CUT);
-            } else {
-                to_pay = count_maximum_coins(client.money, COST_PER_CUT);
+    for (int hairdresser_id = 0; hairdresser_id < NUM_OF_HAIRDRESSERS; hairdresser_id++) {
+        if (fork() == 0) {
+            srand(getpid());
+            log_num("start hairdresser", getpid());
+            for (int i = 0; i < 100; i++) {
+                usleep(rand() % 500000);
+                client client = take_client(waiting_room);
+                log_num("get client", client.id);
+                money to_pay;
+                if (rand() % 2 == 0) {
+                    to_pay = count_minimum_coins(client.money, COST_PER_CUT);
+                } else {
+                    to_pay = count_maximum_coins(client.money, COST_PER_CUT);
+                }
+                add_cash(cash_machine, to_pay);
+                log_num("get money in machine for", client.id);
+                int to_return = get_amount(to_pay) - COST_PER_CUT;
+                usleep(rand() % 500000);
+                log_num("finished client", client.id);
+                money change = cash_machine_change(cash_machine, to_return, hairdresser_id);
+                log_num("get change for", client.id);
+                money to_send = subtract(change, to_pay);
+                log_money("send change", to_send);
+                change_msg_t change_msg;
+                change_msg.client_id = client.id;
+                change_msg.change = to_send;
+                if (msgsnd(client.change_queue, &change_msg, sizeof(change_msg.change), 0) == -1) {
+                    perror("Send client change");
+                    exit(1);
+                }
+                log_num("add change to queue for client", client.id);
+                log_msg("hairdresser finished");
             }
-            add_cash(cash_machine, to_pay);
-            log_num("get money in machine for", client.id);
-            int to_return = get_amount(to_pay) - COST_PER_CUT;
-            usleep(rand() % 500000);
-            log_num("finished client", client.id);
-            money change = cash_machine_change(cash_machine, to_return, hairdresser_id);
-            log_num("get change for", client.id);
-            money to_send = subtract(change, to_pay);
-            log_money("send change", to_send);
-            change_msg_t change_msg;
-            change_msg.client_id = client.id;
-            change_msg.change = to_send;
-            if (msgsnd(client.change_queue, &change_msg, sizeof(change_msg.change), 0) == -1) {
-                perror("Send client change");
-                exit(1);
-            }
-            log_num("add change to queue for client", client.id);
-            log_msg("hairdresser finished");
+            exit(0);
         }
-        exit(0);
     }
 
     // CLIENT
-    if (fork() == 0) {
-        srand(getpid());
-        client client = new_client(client_id, change_queue);
-        log_num("start client", getpid());
-        for (int i = 0; i < 100; i++) {
-            while (get_amount(client.money) < COST_PER_CUT) {
-                make_money(client);
+    for (int client_id = 1; client_id <= NUM_OF_CLIENTS; client_id++) {
+        if (fork() == 0) {
+            srand(getpid());
+            client client = new_client(client_id, change_queue);
+            log_num("start client", getpid());
+            for (int i = 0; i < 100; i++) {
+                while (get_amount(client.money) < COST_PER_CUT) {
+                    make_money(client);
+                }
+                log_num("wait for free seat", client.id);
+                wait_for_free_seat(waiting_room);
+                log_num("get free seat", client.id);
+                take_seat(waiting_room, client);
+                log_num("take seat", client.id);
+                log_num("wait for change", client.id);
+                change_msg_t change_msg;
+                if (msgrcv(change_queue, &change_msg, sizeof(change_msg.change), client_id, 0) == -1) {
+                    perror("Get change for client");
+                    exit(1);
+                }
+                log_num("get change", client.id);
+                client.money.ones += change_msg.change.ones;
+                client.money.twos += change_msg.change.twos;
+                client.money.fives += change_msg.change.fives;
+                log_msg("client finished");
             }
-            log_num("wait for free seat", client.id);
-            wait_for_free_seat(waiting_room);
-            log_num("get free seat", client.id);
-            take_seat(waiting_room, client);
-            log_num("take seat", client.id);
-            log_num("wait for change", client.id);
-            change_msg_t change_msg;
-            if (msgrcv(change_queue, &change_msg, sizeof(change_msg.change), client_id, 0) == -1) {
-                perror("Get change for client");
-                exit(1);
-            }
-            log_num("get change", client.id);
-            client.money.ones += change_msg.change.ones;
-            client.money.twos += change_msg.change.twos;
-            client.money.fives += change_msg.change.fives;
-            log_msg("client finished");
+            exit(0);
         }
-        exit(0);
     }
-    wait(NULL);
-    wait(NULL);
+
+    for (int i = 0; i < NUM_OF_HAIRDRESSERS + NUM_OF_CLIENTS; i++) {
+        wait(NULL);
+    }
 
     return 0;
 }
