cf719e42a63c49bb01bc5b410292ed2b91a89113
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-01-06 19:07:35 +0100

    handle chairs

--- fryzjerzy.c
+++ fryzjerzy.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/msg.h>
+#include <sys/sem.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -15,15 +16,30 @@
 #define SIZE_OF_WAITING_ROOM 2
 #define NUM_OF_HAIRDRESSERS 1
 #define NUM_OF_CLIENTS 1
+#define NUM_OF_CHAIRS 1
 #define COST_PER_CUT 10
 
 #define EMPTY 1
 #define FULL 2
 
+int init_chairs(int num_of_chairs) {
+    int chairs = semget(IPC_PRIVATE, 1, IPC_CREAT | 0600);
+    if (chairs == -1) {
+        perror("Creating chairs");
+        exit(1);
+    }
+    if (semctl(chairs, 0, SETVAL, num_of_chairs) == -1) {
+        perror("Creating chairs");
+        exit(1);
+    }
+    return chairs;
+}
+
 int main(int argc, char const *argv[]) {
     waiting_room waiting_room = init_waiting_room(SIZE_OF_WAITING_ROOM);
     cash_machine cash_machine = init_cash_machine(NUM_OF_HAIRDRESSERS);
     int change_queue = new_change_queue();
+    int chairs = init_chairs(NUM_OF_CHAIRS);
 
     // HAIRDRESSER
     d_log_num("start main", getpid());
@@ -44,7 +60,9 @@ int main(int argc, char const *argv[]) {
                 add_cash(cash_machine, to_pay);
                 d_log_num("get money in machine for", client.id);
                 int to_return = get_amount(to_pay) - COST_PER_CUT;
+                down(chairs, 0);
                 usleep(rand() % 500000);
+                up(chairs, 0);
                 d_log_num("finished client", client.id);
                 money change = cash_machine_change(cash_machine, to_return, hairdresser_id);
                 d_log_num("get change for", client.id);
