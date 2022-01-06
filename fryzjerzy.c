17b7cc0ecbc0d3e1aba7607e57e2edb24390f73f
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-01-06 11:58:46 +0100

    remove unecessary includes

--- fryzjerzy.c
+++ fryzjerzy.c
@@ -1,12 +1,7 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/ipc.h>
 #include <sys/msg.h>
-#include <sys/sem.h>
-#include <sys/shm.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
